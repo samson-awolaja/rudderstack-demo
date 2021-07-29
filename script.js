2f7efb418565d10db84a2dba0348c3a56344b3a3
dalyd14 <dalyd14@gmail.com>
2021-07-29 13:30:39 -0400

    added code to capture email; addded styling

---
+++ script.js
@@ -0,0 +1,11 @@
+// Event listener on the form submit
+document.querySelector("#email-form").addEventListener("submit", function(event){
+    event.preventDefault()
+
+    const emailForm = document.querySelector("#email")
+    const email = emailForm.value
+
+    console.log(email)
+
+    emailForm.value = ""
+} )
